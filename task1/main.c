7f6997964195db9d9e4419f1aac48407151febc3
Guixv588 <1804943480@qq.com>
2022-10-24 16:08:43 +0800

    Add main

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+int main(void)
+{
+	double a,h=0.001,x=0,f=1.0,n=0,m=0;
+	scanf("%lf",&a);
+
+	
+	printf("x=%f y=%f\n",x,a);
+	f*=(1.0-h+h*h/2.0);
+	n=f*a;
+	x+=h;
+	printf("x=%f y=%f\n",x,n);
+	f*=(1.0-h+h*h/2.0);
+	m=f*a;
+	x+=h;
+	printf("x=%f y=%f\n",x,m);
+	while(n-m>=0.00001){
+		n=m;
+		f*=(1.0-h+h*h/2.0);
+		m=f*a;
+		x+=h;
+		printf("x=%f y=%f\n",x,m);
+	}
+
 	return 0;
 }
 
